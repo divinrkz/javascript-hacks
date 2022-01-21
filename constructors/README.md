eab3b1007397fd5b9fa1a83a299c5c203c7e0565
divinirakiza <irakizadivin@gmail.com>
2022-01-21 12:21:16 +0200

    [feat]: add constructor docs

---
+++ constructors/README.md
@@ -0,0 +1,6 @@
+## Javascript Constructors.
+Constructors is used in Javascript to create object with similar properties 
+and methods.
+
+
+
---
+++ constructors/main.js
@@ -0,0 +1,8 @@
+function Student(name, age, mark) {
+    this.name = name;
+    this.age = age;
+    this.mark = mark;
+}
+
+const studentOne = new Student('Dev', 32, 23);
+const studentTwo = new Student('Dev2', 43, 3);
