e8d0c380bd2fc53d99ec844379af3208ba7840ad
Joydip Roy <joydipand@gmail.com>
2022-07-07 16:58:57 +0000

    fix(typo): call-vs-apply-vs-bind

--- call-vs-apply-vs-bind/main.js
+++ call-vs-apply-vs-bind/main.js
@@ -1,53 +1,27 @@
-// call() example
 const personOne = {
-    firstName : "Js",
-    secondName : "Dev"
-  }
-  
-const getFullName = function(company, country) {
-    console.log(this.firstName + " " + this.secondName + ", " + company + ", " + country);
+  firstName: "Js",
+  secondName: "Dev"
 }
-  
+
 const personTwo = {
-    firstName : "Js",
-    secondName : "Dev"
-}
-  
-getFullName.call(personOne, "Tesla", "United States");    // outputs Js Dev, Tesla, United States
-getFullName.call(personTwo, "Facebook", "United States");    // outputs Js Zuckerberg, Facebook, United States
-
-
-//apply() example
-const personThree = {
-    firstName : "Js",
-    secondName : "Dev"
-  }
-  
-const getFullName = function(company, country) {
-    console.log(this.firstName + " " + this.secondName + ", " + company + ", " + country);
+  firstName: "Js",
+  secondName: "ZuckerBerg"
 }
 
-const personFour = {
-    firstName : "Js",
-    secondName : "Dev"
-}
-
-getFullName.apply(personOne, ["Tesla", "United States"]);    // outputs Js Dev, Tesla, United States
-getFullName.apply(personTwo, ["Facebook", "United States"]);    // outputs Js ZuckerBerg
-
-
-//bind() Exanple
-const personFive= {
-    firstName : "Js",
-    secondName : "Dev"
-}
-  
-const getFullName = function(company, country) {
+const getFullName = function (company, country) {
   console.log(this.firstName + " " + this.secondName + ", " + company + ", " + country);
 }
-  
-const getPersonFiveDetails = getFullName.bind(personFive, ["Tesla", "United States"]); 
-getPersonFiveDetails();
 
-// calling the bind method on 'getFullName' creates a copy of it, which is assigned to 'getPersonFiveDetails'.
-//  When the 'getPersonFiveDetails' is called separately, we will get the desired output
+// call() example
+getFullName.call(personOne, "Tesla", "United States");       // Output: Js Dev, Tesla, United States
+getFullName.call(personTwo, "Facebook", "United States");    // Output: Js Zuckerberg, Facebook, United States
+
+//apply() example
+getFullName.apply(personOne, ["Tesla", "United States"]);       // Output: Js Dev, Tesla, United States
+getFullName.apply(personTwo, ["Facebook", "United States"]);    // Output: Js ZuckerBerg
+
+// bind() Exanple
+const getPersonFiveDetails = getFullName.bind(personOne, ["Tesla", "United States"]);
+// Calling the bind method on 'getFullName' creates a copy of it, which is assigned to 'getPersonFiveDetails'.
+// When the 'getPersonFiveDetails' is called separately, we will get the desired output
+getPersonFiveDetails(); 
